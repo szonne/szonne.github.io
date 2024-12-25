7bbba2c8d261bf4937484ef9c7c0a98cc9df6f35
Seong Jong Han <seungjong0222@gmail.com>
2024-12-26 06:11:31 +0900

    Add post

--- _posts/2024-12-25-doubly-linked-list.md
+++ _posts/2024-12-25-doubly-linked-list.md
@@ -13,4 +13,14 @@ toc_label: "On This Page"
 ![이중연결리스트](/assets/images/doubly-linked-list.png "Doubly Linked List")
 
 이중 연결 리스트는 단순 연결 리스트가 갖고 있는 단점을 보완한다.
-단순 연결 리스트가 단방향으로 노드들이 연결되어 있는 것에 반해, 이중 연결 리스트는 이름 그대로 노드들이 양방향으로 연결되어 있다.  
+단순 연결 리스트가 단방향으로 노드들이 연결되어 있는 것에 반해, 이중 연결 리스트는 이름 그대로 노드들이 양방향으로 연결되어 있다.
+
+### 구현 (Python)
+- 노드는 단순 연결 리스트와 다르게, 다음 노드와 이전 노드를 가리키는 레퍼런스를 알아야 한다. 
+```python
+class Node:
+    def __init__(self, data, prev, next):
+        self.data = data
+        self.prev = prev
+        self.next = next
+```
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Doubly Linked List (이중연결리스트)",
-        "excerpt":"이중 연결 리스트 (Doubly Linked List)      이중 연결 리스트는 단순 연결 리스트가 갖고 있는 단점을 보완한다. 단순 연결 리스트가 단방향으로 노드들이 연결되어 있는 것에 반해, 이중 연결 리스트는 이름 그대로 노드들이 양방향으로 연결되어 있다.  ","categories": ["data-structure"],
+        "excerpt":"이중 연결 리스트 (Doubly Linked List) 이중 연결 리스트는 단순 연결 리스트가 갖고 있는 단점을 보완한다. 단순 연결 리스트가 단방향으로 노드들이 연결되어 있는 것에 반해, 이중 연결 리스트는 이름 그대로 노드들이 양방향으로 연결되어 있다. 구현 (Python) 노드는 단순 연결 리스트와 다르게, 다음 노드와 이전 노드를 가리키는 레퍼런스를 알아야 한다. class...","categories": ["data-structure"],
         "tags": ["data-structure"],
         "url": "/data-structure/doubly-linked-list/",
         "teaser": null
